6ab8b136a37680db057568257e4277b8c2dcda21
friskilars <friskilasimarmata47@gmail.com>
2024-11-15 11:41:49 +0700

    first commit

---
+++ scripts.js
@@ -0,0 +1,5 @@
+window.addEventListener("load", function() {
+    document.querySelector('.content-paragraph').classList.add('slide-in-right');
+    document.querySelector('.intro-text').classList.add('slide-in-right');
+    document.querySelector('.img-fluid').classList.add('slide-in-left');
+});
